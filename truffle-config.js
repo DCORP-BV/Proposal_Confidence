652cc10bf5678222e8f4724c8e0e55aa7e90217c
DecentralizedIT <frank@dcorp.it>
2019-08-08 14:12:33 +0700

    Mainnet config

--- truffle-config.js
+++ truffle-config.js
@@ -39,13 +39,13 @@ module.exports = {
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
-
-    // Useful for private networks
-    // private: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-      // network_id: 2111,   // This network is yours, in the cloud.
-      // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
+    mainnet: {
+      provider: () => new HDWalletProvider(secret.main, `https://mainnet.infura.io/v3/42603e7e72bf4278ab055f3385efc2de`),
+      network_id: 1,       // Mainnet's id
+      gas: 5500000,        // Same as test network
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200  // # of blocks before a deployment times out  (minimum/default: 50)
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
